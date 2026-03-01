333bcd0a0fa8f70e5aa1e886672d6a43d945cc49
Ahmed Mahmoud <51807333+AhmedMahmoud2@users.noreply.github.com>
2026-03-01 19:39:29 +0000

    Delete migrations/V001__use_target_context.sql

--- migrations/V001__use_target_context.sql
+++
@@ -1,6 +0,0 @@
--- V001__use_target_context.sql
--- NHS-grade: database + schema are created once by an admin.
--- This migration only sets context.
-
-USE DATABASE IDENTIFIER($SNOWFLAKE_DATABASE);
-USE SCHEMA IDENTIFIER($SNOWFLAKE_SCHEMA);
